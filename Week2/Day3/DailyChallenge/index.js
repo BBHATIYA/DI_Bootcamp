9d44b73296efa193804dbfc91039bf12ebbb6389
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-06-28 15:37:48 +0300

    adding DailyChallenge week2day3

---
+++ Week2/Day3/DailyChallenge/index.js
@@ -0,0 +1,19 @@
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+
+let star = "*"
+for (let i=0; i<7; i++ ){
+    console.log("* ".repeat(i));
+}
+
+
+
+for (let counter=0; counter<7; counter++){
+    let newStar = "";
+    for(let count=0; count<counter; count++){
+        newStar += "* ";
+    }
+    console.log(newStar);
+}
