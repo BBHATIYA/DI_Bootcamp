6cbe5020a1d7fc4c02fe8a2fe01600d8952d4a29
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-06-30 10:00:57 +0300

    adding dailychallent

--- Week2/Day4/DailyChallenge/script.js
+++ Week2/Day4/DailyChallenge/script.js
@@ -1,25 +1,53 @@
-let userAnswer = "";
- userAnswer = prompt("Give us several words (separated by commas)");
+// let userAnswer = "";
+//  userAnswer = prompt("Give us several words (separated by commas)");
 
-// console.log(userAnswer);
-let strings;
-strings = userAnswer;
-let usingSplit = strings.split(',');
-// console.log(usingSplit);
+// // console.log(userAnswer);
+// let strings;
+// strings = userAnswer;
+// let usingSplit = strings.split(',');
+// // console.log(usingSplit);
 
 
-let width = 0;
-for (let i = 0; i < usingSplit.length; i++){
-  if (usingSplit[i].length > width){
-       width = usingSplit[i].length
+
+
+
+let words = ["Hello", "World", "in", "a", "frame"];
+
+function longestWordLength(arr) {
+  let len = arr[0].length;
+  for (let i = 1; i < words.length; i++) {
+      if(arr[i].length > len){
+        len = arr[i].length;
+      }
   }
+  return len;
 }
-  for (let i = 0; i<= usingSplit.length; i++){
-    if((i == 0) || (i == usingSplit.length)){
-      console.log("*".repeat(width))
-      continue;
-    }
-    console.log("*" +usingSplit[i]+ +"\n ".repeat(usingSplit[i].length)+"*");
-  }
 
-  //didn't succeed to print as per instruction
+function addSpaces(word,len){
+  if(word.length == len){
+    return "";
+  }
+  return " ".repeat(len-word.length);
+}
+
+function stars () {
+  let len = longestWordLength(words);
+  console.log("*".repeat(len+4));
+  for(x in words){
+    console.log('* ' + words[x] + addSpaces(words[x],len) + ' *');
+  }
+  console.log("*".repeat(len+4));
+}
+stars();
+
+
+
+
+
+
+
+
+
+
+
+
