c2d02b173976f9fa2b5585ceed35574cbe02ab2b
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-07-12 08:15:10 +0300

    edit dailychallenge day2

--- Week4/Day2/DailyChallenge/script.js
+++ Week4/Day2/DailyChallenge/script.js
@@ -16,9 +16,9 @@ let client = "Betty";
 let groceries = {
   fruits: ["pear", "apple", "banana"],
   vegetables: ["tomatoes", "cucumber", "salad"],
-  totalPrice: "35$",
+  totalPrice: "20$",
   other: {
-    payed: false,
+    payed: true,
     meansOfPayment: ["cash", "creditCard"],
   },
 };
@@ -29,14 +29,19 @@ const displayGroceries = groceries.fruits.forEach((params) => {
 
 const cloneGroceries = () => {
   let user = client;
-  //   console.log(user);
+  // user = "Betty";
+  console.log(user);
   let shopping = groceries;
   console.log(shopping);
+  let newPrice = (groceries.totalPrice = "35$");
+  console.log(newPrice);
+  let playedNewValue = (groceries.other.payed = "false");
+  console.log(playedNewValue);
 };
 cloneGroceries();
 // Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
-// Yes...because user is = clint and we change value to the first address
+// Yes...because user is = clint and Pass By Reference
 
-// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ? Yes...----because shopping is = groceries and we change value to the first address
+// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ? Yes...----because shopping is = groceries and we change value to the first address..it's Pass By Reference
 
-// Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ? Yes..same reason as above changing value in first address and we made shopping = groceries
+// Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ? Yes..same reason as above changing value in first address and we made shopping = groceries - Pass By Reference
