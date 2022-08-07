9c541882801411487b01ea3a8ef48449e1317642
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-08-07 17:00:38 +0300

    adding dailychallenge

---
+++ Week8/Day1/DailyChallenge/images/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import the carousel
+import DemoCarousel from "./DemoCarousel";
+
+function App() {
+  return (
+    <div className="App">
+      <DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week8/Day1/DailyChallenge/images/src/DemoCarousel.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+class DemoCarousel extends Component {
+  render() {
+    const style = {
+      margin: "auto",
+      width: "500px",
+    };
+    return (
+      <div style={style}>
+        <Carousel>
+          <div>
+            <img
+              src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"
+              alt="city"
+            />
+            <p className="legend">Legend 1</p>
+          </div>
+          <div>
+            <img
+              src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"
+              alt="city"
+            />
+            <p className="legend">Legend 2</p>
+          </div>
+          <div>
+            <img
+              src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"
+              alt="city"
+            />
+            <p className="legend">Legend 3</p>
+          </div>
+          <div>
+            <img
+              src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"
+              alt="city"
+            />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default DemoCarousel;
